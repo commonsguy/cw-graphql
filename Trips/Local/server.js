c3855716954cd78c26001627289d30b15c11fe76
Mark Murphy <mmurphy@commonsware.com>
2017-07-24 10:03:16 -0400

    test server supports 0.1/ and 0.2/ in URLs

--- Trips/Local/server.js
+++ Trips/Local/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var v0p1 = require('./server-v0.1.js')
+var v0p2 = require('./server-v0.2.js')
 
 app.use(v0p1.route(), v0p1.handler());
+app.use(v0p2.route(), v0p2.handler());
 app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000'+v0p1.route());
+console.log('Running a GraphQL API server at localhost:4000');
